07507e3b3d39f2f10490990f848c9197960fed79
bas <bas@noisyoyster.net>
2015-02-06 21:12:05 +1100

    adding stuff

---
+++ CameraRecordTest/SnapTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using CameraRecord;
+namespace CameraRecordTest
+{
+    [TestFixture]
+    public class SnapTest
+    {
+        [Test]
+        public void TestCaptureDate()
+        {
+            Snap click = new Snap();
+
+            DateTime nownow = new DateTime();
+            click.CaptureDate = nownow;
+
+            Assert.AreEqual(nownow, click.CaptureDate);
+
+
+        }
+    }
+}
